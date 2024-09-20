076b545f56f31add208f00ec9b0efaa6190099cd
Xavier Hampton <xaviermichaelhampton@gmail.com>
2024-09-19 23:57:45 -0500

    UI layout for custom food creation

--- components/CreateFoodModal.js
+++ components/CreateFoodModal.js
@@ -20,16 +20,35 @@ export default class CreateFoodModal extends React.Component {
             <View style={styles.modalContent}>
             
 
-              <View style={styles.titleContainer}></View>
-              <View style={styles.titleContainer}></View>
-              <View style={styles.titleContainer}></View>
-              <View style={styles.titleContainer}></View>
-              <View style={styles.titleContainer}></View>
+              <View style={styles.titleContainer}>
+                <Text style={styles.headerText}>Custom Food</Text>
+              </View>
+
+              <View style={{display: 'block', width: '100%'}}>
+                <Text style={styles.subHeaderText}>Food Information</Text>
+              </View>
+              <View style={styles.foodInformationContainer}>
+
+              </View>
+
+
+              <View style={{display: 'block', width: '100%'}}>
+                <Text style={styles.subHeaderText}>Food Macros</Text>
+              </View>
+              <View style={styles.foodMacrosContainer}>
+
+              </View>
+
+
+              
 
 
 
             </View>
           </ScrollView>
+          <Pressable style={styles.createFoodButton}>
+              <Text style={styles.createFoodButtonText}>Create Food</Text>
+          </Pressable>
           </View>
             </Modal>
         )
@@ -62,8 +81,67 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     width: 340,
-    height: 70,
+    height: 40,
     backgroundColor: themeColor().secondary,
     borderRadius: 10,
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  headerText: {
+    color: 'white',
+    fontFamily: 'JetBrainsMono',
+    fontSize: 20,
+    opacity: .6,
+  },
+  subHeaderText: {
+    color: 'white',
+    fontFamily: 'JetBrainsMono',
+    fontSize: 16,
+    paddingLeft: 10,
+    marginBottom: -10,
+  },
+  foodInformationContainer: {
+    width: 360,
+    height: 200,
+    backgroundColor: themeColor().secondary,
+    
+    borderRadius: 10,
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  foodMacrosContainer: {
+    width: 360,
+    height: 300,
+    backgroundColor: themeColor().secondary,
+    
+    borderRadius: 10,
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  createFoodButton: {
+    width: 320,
+    height: 60,
+    borderRadius: 10,
+    position: 'absolute',
+    backgroundColor: 'green',
+    bottom: 40,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  createFoodButtonText: {
+    color: 'white',
+    fontFamily: 'JetBrainsMono',
+    fontSize: 20,
   }
+
 })
