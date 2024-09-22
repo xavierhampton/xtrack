2f616e3c94fc685f20e2fb84d605d9f5d1504f49
Xavier Hampton <xaviermichaelhampton@gmail.com>
2024-09-21 23:58:12 -0500

    Added text inputs for required values for custom

--- components/CreateFoodModal.js
+++ components/CreateFoodModal.js
@@ -1,6 +1,7 @@
-import { Modal, View, Text, Pressable, StyleSheet, ScrollView, TextInput } from 'react-native';
-import React, {useState, useEffect, useRef} from 'react';
+import { Modal, View, Text, Pressable, StyleSheet, ScrollView, TextInput, KeyboardAvoidingView } from 'react-native';
+import React, {useState, useEffect} from 'react';
 import {themeColor} from '@/hooks/theme'
+import { displayPartsToString } from 'typescript';
 
 export default class CreateFoodModal extends React.Component {
     constructor(props) {
@@ -28,15 +29,42 @@ export default class CreateFoodModal extends React.Component {
                 <Text style={styles.subHeaderText}>Food Information</Text>
               </View>
               <View style={styles.foodInformationContainer}>
+                  
+                  <View style={[styles.flexContainer, {height: 60, width: 350}]}>
+                    <Text style={styles.label}>Food Name</Text>
+                    <TextInput style={styles.textInput}></TextInput>
+                  </View>
+
+                  <View style={styles.flexContainer}>
+
+                  </View>
+
 
               </View>
-
-
               <View style={{display: 'block', width: '100%'}}>
                 <Text style={styles.subHeaderText}>Food Macros</Text>
               </View>
               <View style={styles.foodMacrosContainer}>
-
+                  <View style={[styles.flexContainer, {height: 50, width: 350}]}>
+                    <Text style={styles.label}>Calories</Text>
+                    <TextInput style={[styles.textInput, {width: 80, textAlign: 'center'}]}></TextInput>
+                    <Text style={[styles.subHeaderText, {width: 60, textAlign: 'center', height: 30}]}>kcal</Text>
+                  </View>
+                  <View style={[styles.flexContainer, {height: 50, width: 350}]}>
+                    <Text style={styles.label}>Carbs</Text>
+                    <TextInput style={[styles.textInput, {width: 60, textAlign: 'center'}]}></TextInput>
+                    <Text style={[styles.subHeaderText, {width: 30, textAlign: 'center', height: 30}]}>g</Text>
+                  </View>
+                  <View style={[styles.flexContainer, {height: 50, width: 350}]}>
+                    <Text style={styles.label}>Protein</Text>
+                    <TextInput style={[styles.textInput, {width: 60, textAlign: 'center'}]}></TextInput>
+                    <Text style={[styles.subHeaderText, {width: 30, textAlign: 'center', height: 30}]}>g</Text>
+                  </View>
+                  <View style={[styles.flexContainer, {height: 50, width: 350}]}>
+                    <Text style={styles.label}>Fat</Text>
+                    <TextInput style={[styles.textInput, {width: 60, textAlign: 'center'}]}></TextInput>
+                    <Text style={[styles.subHeaderText, {width: 30, textAlign: 'center', height: 30}]}>g</Text>
+                  </View>
               </View>
 
 
@@ -98,6 +126,7 @@ const styles = StyleSheet.create({
   },
   subHeaderText: {
     color: 'white',
+    opacity: .6,
     fontFamily: 'JetBrainsMono',
     fontSize: 16,
     paddingLeft: 10,
@@ -106,11 +135,9 @@ const styles = StyleSheet.create({
   foodInformationContainer: {
     width: 360,
     height: 200,
-    backgroundColor: themeColor().secondary,
     
     borderRadius: 10,
     display: 'flex',
-    justifyContent: 'center',
     alignContent: 'center',
     alignItems: 'center',
     marginBottom: 20,
@@ -118,11 +145,11 @@ const styles = StyleSheet.create({
   foodMacrosContainer: {
     width: 360,
     height: 300,
-    backgroundColor: themeColor().secondary,
+
+    gap: 10,
     
     borderRadius: 10,
     display: 'flex',
-    justifyContent: 'center',
     alignContent: 'center',
     alignItems: 'center',
     marginBottom: 20,
@@ -132,7 +159,7 @@ const styles = StyleSheet.create({
     height: 60,
     borderRadius: 10,
     position: 'absolute',
-    backgroundColor: 'green',
+    backgroundColor: '#306844',
     bottom: 40,
     display: 'flex',
     justifyContent: 'center',
@@ -142,6 +169,33 @@ const styles = StyleSheet.create({
     color: 'white',
     fontFamily: 'JetBrainsMono',
     fontSize: 20,
+  },
+  flexContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    backgroundColor: themeColor().secondary,
+    borderRadius: 10,
+  },
+  label: {
+    fontFamily: 'JetBrainsMono',
+    color: 'white',
+    fontSize: 20,
+    marginLeft: 15,
+  },
+  textInput: {
+    width: 200,
+    borderColor: 'white',
+    borderWidth: 2,
+    height: 40,
+    borderRadius: 5,
+    marginLeft: 'auto',
+    color: 'white',
+    fontSize: 16,
+    fontFamily: 'JetBrainsMono'
   }
+
 
 })
