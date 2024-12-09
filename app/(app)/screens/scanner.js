31f77bacfe47ee2c1fda19c0601e0775db50ccb2
Xavier Hampton <xaviermichaelhampton@gmail.com>
2024-12-08 20:47:52 -0600

    implemented scanner.js file to scan barcodes

--- app/(app)/screens/scanner.js
+++ app/(app)/screens/scanner.js
@@ -1,38 +1,58 @@
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
+import { CameraView, CameraType, useCameraPermissions, launchScanner } from 'expo-camera';
+import { useEffect, useState } from 'react';
+import {themeColor} from '@/hooks/theme'
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const scanner = (props) => {
   const [facing, setFacing] = useState('back');
   const [permission, requestPermission] = useCameraPermissions();
+  const [header, setHeader] = useState('Scanning for barcodes.')
+
+  useEffect(() => {
+    const loadingCycle = ['Scanning for barcodes.', 'Scanning for barcodes..', 'Scanning for barcodes...'];
+    let index = 0;
+
+    const interval = setInterval(() => {
+      index = (index + 1) % loadingCycle.length;
+      setHeader(loadingCycle[index]);
+    }, 500); 
+
+    
+    return () => clearInterval(interval);
+  }, []);
 
   if (!permission) {
     // Camera permissions are still loading.
-    return <View />;
+    return <View style={{backgroundColor: themeColor().secondary}} />;
   }
+
+  
 
   if (!permission.granted) {
     // Camera permissions are not granted yet.
     return (
-      <View style={styles.container}>
-        <Text style={styles.message}>We need your permission to show the camera</Text>
+      <View style={[styles.container, themeColor().secondary]}>
+        <Text style={styles.message}>xTrack Needs Camera Permissions to Scan Barcodes</Text>
         <Button onPress={requestPermission} title="grant permission" />
       </View>
     );
   }
 
-  function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
-  }
+
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
+      <CameraView barcodeScannerSettings={{barcodeTypes: ['ean13']}} onBarcodeScanned={(e) => {console.log(e.data)}}style={styles.camera} facing={facing}>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.text}>x</Text>
           </TouchableOpacity>
+          
         </View>
+        <View style={[styles.buttonContainer, {justifyContent: 'center'}]}>
+          <Text style={styles.header}>{header}</Text>
+        </View>
+       
       </CameraView>
     </View>
   );
@@ -50,21 +70,36 @@ const styles = StyleSheet.create({
   },
   camera: {
     flex: 1,
+    display: 'flex',
+    flexDirection: 'column'
   },
   buttonContainer: {
-    flex: 1,
     flexDirection: 'row',
     backgroundColor: 'transparent',
-    margin: 64,
+    margin: 0,
   },
   button: {
-    flex: 1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
+    alignSelf: 'flex-start',
+    alignItems: 'flex-end',
+
+    marginLeft: 'auto',
+    marginRight: 30,
+    marginTop: 30,
+
+    width: 50,
+    height: 50,
   },
   text: {
-    fontSize: 24,
+    fontSize: 50,
     fontWeight: 'bold',
     color: 'white',
+
   },
+  header: {
+    fontFamily: 'JetBrainsMono',
+    fontSize: 18,
+    textAlign: 'center',
+    color: 'white',
+    marginTop: 20
+  }
 });
